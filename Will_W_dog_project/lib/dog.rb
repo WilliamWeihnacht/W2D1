09854e6de2467e1b7b246097dd7023df05faeb2d
Will Weihnacht <wwhynot@terpmail.umd.edu>
2022-10-10 14:46:38 -0700

    added some stuff

--- Will_W_dog_project/lib/dog.rb
+++ Will_W_dog_project/lib/dog.rb
@@ -2,7 +2,29 @@ class Dog
 
     def initialize(name,breed,age,bark,favorite_foods)
         @name = name
-        @bre
+        @breed = breed
+        @age = age
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        name
+    end
+
+    def breed
+        breed
+    end
+
+    def age
+        age
+    end
+
+    def bark
+        bark
+    end
+    
+    def favorite_foods
+        favorite_foods
     end
 
 end
