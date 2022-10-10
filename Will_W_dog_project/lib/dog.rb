399dc4a2bc0479e56171ff3b3664b3a9d72c0be9
Will Weihnacht <wwhynot@terpmail.umd.edu>
2022-10-10 15:09:52 -0700

    done with dog.rb

--- Will_W_dog_project/lib/dog.rb
+++ Will_W_dog_project/lib/dog.rb
@@ -3,28 +3,39 @@ class Dog
     def initialize(name,breed,age,bark,favorite_foods)
         @name = name
         @breed = breed
+        @bark = bark
         @age = age
         @favorite_foods = favorite_foods
     end
 
     def name
-        name
+        @name
     end
 
     def breed
-        breed
+        @breed
     end
 
     def age
-        age
+        @age
     end
 
     def bark
-        bark
+        return @bark.upcase if @age > 3
+        return @bark.downcase
     end
     
     def favorite_foods
-        favorite_foods
+        @favorite_foods
+    end
+
+    def age=(num)
+        @age = num
+    end
+
+    def favorite_food?(food)
+        return true if favorite_foods.include?(food.capitalize)
+        return false
     end
 
 end
